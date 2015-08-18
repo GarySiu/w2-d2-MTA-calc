9524688a2610b9e66f473c691f2af096d3c658f2
Gary Siu <Gary@untypeable.com>
2015-08-18 18:28:57 +0100

    Skeleton layout done with some placeholder text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-//Store the stations in an array for each line
-var n_line = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"];
-var l_line = ["8th","6th","Union Square","3rd","1st"]
-var 6_line = ["Grand Central","33rd","28th","23rd","Union Square","Astor Place"]
+// OLD IDEA. Do not use. Also you can't name your variables starting with a number stupid!
+// //Store the stations in an array for each line
+// var n_line = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"];
+// var l_line = ["8th","6th","Union Square","3rd","1st"]
+// var 6_line = ["Grand Central","33rd","28th","23rd","Union Square","Astor Place"]
