d7c6d2b2b7d139d6c917f635e29aa188add15196
Gary Siu <Gary@untypeable.com>
2015-08-18 17:53:14 +0100

    Wait. What if we store the data in the HTML instead? Also boilerplate Box Sizing fix to CSS

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//Store the stations in an array for each line
+var n_line = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"];
+var l_line = ["8th","6th","Union Square","3rd","1st"]
+var 6_line = ["Grand Central","33rd","28th","23rd","Union Square","Astor Place"]
