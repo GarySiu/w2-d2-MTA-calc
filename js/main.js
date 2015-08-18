61ea6db8b584c8a0ab59dcf2c19814e94a34a035
Gary Siu <Gary@untypeable.com>
2015-08-18 19:57:33 +0100

    Well... now it stores station choices

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 // OLD IDEA. Do not use. Also you can't name your variables starting with a number stupid!
+//
 // //Store the stations in an array for each line
 // var n_line = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"];
 // var l_line = ["8th","6th","Union Square","3rd","1st"]
 // var 6_line = ["Grand Central","33rd","28th","23rd","Union Square","Astor Place"]
+
+window.onload = function() {
+  startListeners();
+  enumStepTargets();
+}
+var step = 0;
+function enumStepTargets() {
+  var step1 = document.getElementById('step1');
+  var step2 = document.getElementById('step2');
+  var step3 = document.getElementById('step3');
+  var step4 = document.getElementById('step4');
+}
+function setChoice() {
+  switch(step) {
+    case 0: step1.innerText = this.innerText;
+    step++;
+    break;
+    case 1: step2.innerText = this.innerText;
+    step++;
+    break;
+    // case 2: step3.innerText = this.innerText;
+    // step++;
+    // break;
+    // case 3: step4.innerText = this.innerText;
+    // step++;
+    // break;
+  }
+}
+
+function startListeners() {
+//Stick listeners to all the stops on the nLine
+for (var i = 0; i < document.getElementsByClassName('nLineStops').length; i++) {
+  document.getElementsByClassName('nLineStops')[i].addEventListener('click', setChoice)
+  }
+}
